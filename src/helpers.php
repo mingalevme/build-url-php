3af29ce8fa5797648d95fcdaf8f7cb27d99ea8f3
Mingalev Mikhail <mingalevme@gmail.com>
2019-03-27 10:18:23 +0300

    Fixed build_url's annotation

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,7 @@ if (! function_exists('build_url')) {
     /**
      * Build an URL
      *
-     * @param $url Base url, to build from scratch use any falsy value, e.g. null or ''
+     * @param string $url Base url, to build from scratch use any falsy value, e.g. null or ''
      * @param array $replacement [optional] Associative array like parse_url() returns
      * @param array &$newUrlParts [optional] If set, it will be filled with the parts of the composed url like parse_url() would return
      * @return string The new URL string
